0852276ce495cd5ddb48cb2bbc9c98f31109fdac
Gor <61106836+TheGor-365@users.noreply.github.com>
2023-02-27 22:35:14 +0300

    Add turbo popup logic code

--- app/javascript/controllers/modals_controller.js
+++ app/javascript/controllers/modals_controller.js
@@ -3,5 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="modals"
 export default class extends Controller {
   connect() {
+    console.log("Popup")
+  }
+
+  close(e) {
+    e.preventDefault();
+
+    const modal = document.getElementById("modal");
+    modal.innerHTML = "";
+    modal.removeAttribute("src");
+    modal.removeAttribute("complete");
   }
 }
